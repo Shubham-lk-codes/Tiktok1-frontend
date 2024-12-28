131ffa6a484ce6537f86c9498550e9239e1fa2f1
Shubham-lk-codes <shubhamlonkar137@gmail.com>
2024-12-28 08:46:28 +0530

    Updated CORS to allow Authorization headers

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://tiktok1-backend.onrender.com/api',
+  withCredentials: true, // Include credentials (like cookies or authorization headers) in cross-origin requests
 });
 
 // Add Authorization Token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
 export default API;
-
