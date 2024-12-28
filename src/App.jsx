36057ab7421ca06e5fe317c49fb34e1cd53d184e
Shubham-lk-codes <shubhamlonkar137@gmail.com>
2024-12-28 10:19:28 +0530

    changed routes to lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -13,19 +13,19 @@ import Profile from "./pages/Profile";
 import UsersList from "./pages/UsersList";
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/UnauthHome" element={<Home />} />
+        <Route path="/unauthhome" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/upload" element={<UploadVideo />} />
-        <Route path="/VideoPage" element={<VideoPage />} />
-        <Route path="AuthHome" element={<AuthHome />} />
+        <Route path="/videopage" element={<VideoPage />} />
+        <Route path="/authhome" element={<AuthHome />} />
         <Route path="/" element={<UnuthHome />} />
-        <Route path="/Profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/users" element={<UsersList />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
